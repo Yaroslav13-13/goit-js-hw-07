6d38992ac111b57080a6bb747492eb2e9648dce2
Yaroslav13-13 <frilanserwork13@gmail.com>
2025-06-21 21:09:00 +0300

    HW-JS-07

--- js/task-1.js
+++ js/task-1.js
@@ -1,2 +1,10 @@
-const listCategories = document.querySelectorAll("#categories")
-console.log('number');
+const listCategories = document.querySelectorAll("#categories .item");
+console.log(`Number of categories: ${listCategories.length} `);
+
+listCategories.forEach((item) => {
+  const category = item.querySelector("h2").textContent;
+  const elements = item.querySelectorAll("li").length;
+
+  console.log(`Category: ${category}`);
+  console.log(`Elements: ${elements}`);
+});
