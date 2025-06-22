42a559d395b6e22d363fde353a7a6415ae2eed81
Yaroslav13-13 <frilanserwork13@gmail.com>
2025-06-22 21:22:21 +0300

    HW-JS-07

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,5 @@
 const input = document.querySelector("#name-input");
-// console.log(input);
 const output = document.querySelector("#name-output");
-// console.log(output);
 
 input.addEventListener("input", () => {
   const trimmedValue = input.value.trim();
